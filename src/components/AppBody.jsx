730fe4a091e05b832cce95ecfaeb4a8fe517973d
BriranSus <alexander.susanto001@binus.ac.id>
2025-09-22 08:55:27 +0700

    Added Add Function

--- src/components/AppBody.jsx
+++ src/components/AppBody.jsx
@@ -9,13 +9,29 @@ class AppBody extends React.Component {
         this.state = {
             items: getInitialData(),
         }
+
+        this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
+    }
+
+    onAddNoteHandler({ title, body }){
+        this.setState((prevState) => {
+            return {
+                items: [
+                    ...prevState.items, {
+                        id: +new Date(),
+                        title,
+                        body, 
+                    }
+                ]
+            }
+        })
     }
 
     render() {
         return (
             <div className='note-app__body'>
                 <h2>Buat Catatan</h2>
-                <InputBody></InputBody>
+                <InputBody addNote={this.onAddNoteHandler}></InputBody>
                 <h2>Catatan Aktif</h2>
                 <ItemList items={this.state.items}/>
             </div>
--- src/components/InputBody.jsx
+++ src/components/InputBody.jsx
@@ -13,10 +13,11 @@ class InputBody extends React.Component {
         }
 
         this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+        this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
+        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
 
     }
 
-    //test
     onTitleChangeEventHandler(event){
         const input = event.target.value;
         const limit = 50;
@@ -35,22 +36,41 @@ class InputBody extends React.Component {
                     error: "Sudah melebihi limit"
                 }
             })
-        }
+        }   
+    }
 
-        
-        
+    onBodyChangeEventHandler(event){
+        this.setState(() => {
+            return {
+                body: event.target.value,
+            }
+        })
+    }
+
+    onSubmitEventHandler(event){
+        event.preventDefault();
+        this.props.addNote(this.state);
+        this.setState(() => {
+            return {
+                id: '',
+                title: '',
+                body: '',
+                counter: 50,
+                error: '',
+            }
+        })
     }
 
     render() {
         return (
             <div className='note-input'>
-                <form action="">
+                <form action="" onSubmit={this.onSubmitEventHandler}>
                     <div className='note-input__limit'>
                         <p className='note-input__limit__error'>{this.state.error}</p> 
                         <p>Sisa karakter: {this.state.counter}</p>   
                     </div>
                     <input type="text" placeholder='Ini adalah judul...' value={this.state.title} onChange={this.onTitleChangeEventHandler}/>
-                    <textarea name="" id="" placeholder='Tuliskan catatanmu di sini...' ></textarea>
+                    <textarea name="" id="" placeholder='Tuliskan catatanmu di sini...' value={this.state.body} onChange={this.onBodyChangeEventHandler}></textarea>
                     <button type='submit'>Buat</button>
                 </form>    
             </div>
